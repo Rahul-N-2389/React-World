54cc07248e1b05b46d8b9c16eafe6eed3838e7ed
Rahul-N-2389 <rahulneelam2389@gmail.com>
2026-05-01 17:58:22 +0530

    React first day

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import StudentProfile from "./Components/StudentProfile"
+import StudentData from "./StudentData"
+function App() {
+
+
+  return (
+    //     <div>
+    //   <h1>putta</h1>
+    //   <h1>lokjhgfd</h1>
+    //   <h1>{50%2==0?"even":"odd"}</h1>
+    // </div>
+
+    <div>
+      <StudentProfile />
+      <StudentData />
+      <StudentProfile />
+    </div>
+  )
+}
+export default App
+
---
+++ src/Components/StudentProfile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+class StudentProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 style={{ color: "red" }}>React world</h1>
+                <h1 style={{ color: "red" }}>React world</h1>
+            </div>
+        )
+
+    }
+}
+export default StudentProfile
